3ecc4dfa0ef31520d3ffb34a8e4a2a2e5db79adf
Hocto <orkun.cavdar@gmail.com>
2021-11-28 14:47:06 +0300

    comment for parsing 2

--- index.js
+++ index.js
@@ -46,9 +46,9 @@ app.get("/currency/:from/:to/:amount", (req, res) => {
     )
     .then((response) => {
       console.log("Response time: " + (new Date().getTime() - millis));
-      const html = response.data;
+      /*const html = response.data;
       const $ = cheerio.load(html);
-      /*$(".result__BigRate-sc-1bsijpp-1", html).each(function () {
+      $(".result__BigRate-sc-1bsijpp-1", html).each(function () {
         const money = $(this).text().split(" ")[0];
         const fromFormatter = formatter(from);
         result = fromFormatter.format(amount);
